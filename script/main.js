669128f6b3ec7e04d81dec45f628b4c1686b0fe3
unknown <togatecomz.mj@gmail.com>
2025-07-01 17:38:10 +0900

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("clickMe");
+  button.addEventListener("click", () => {
+    alert("버튼을 눌렀어요!");
+  });
+});
