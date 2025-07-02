0f613f586c47cf663e22a2fc629f65a414be4b7f
unknown <togatecomz.mj@gmail.com>
2025-07-02 14:55:00 +0900

    헤더, 메인

--- script/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("clickMe");
-  button.addEventListener("click", () => {
-    alert("버튼을 눌렀어요!");
-  });
-});
